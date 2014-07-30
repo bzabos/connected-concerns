3fd294485e375e742be2b1aa1fd52b2880871ecc
bzabos <b.zabos@gmail.com>
2014-07-30 14:36:11 -0600

    Added half-baked abstract nodes.

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -57,6 +57,7 @@
 
   mapGraph = new Datamap({
     element: document.getElementById('graph'),
+    projection: 'mercator',
     fills: {
       defaultFill: "#ABDDA4"
 //      win: '#0fa0fa'
@@ -74,7 +75,7 @@
 
   d3.json("/json/graph.json", function(error, graph) {
 
-    var filteredLinks = _(graph.links).chain()
+    filteredLinks = _(graph.links).chain()
         .map(function (link) {
 
           link.origin = _(graph.nodes).findWhere({id: link.source}).geo;
@@ -85,12 +86,17 @@
         .filter(function (v) {return !!v})
         .value();
 
-    mapGraph.arc(filteredLinks, {strokeWidth: 1, arcSharpness: 1.4});
+    abstractLinks = _(graph.links).filter(function (v) {return !v.origin || !v.destination});
+
+    var arcOptions = {strokeWidth: 1, arcSharpness: 1.4};
+    mapGraph.arc(filteredLinks, arcOptions);
 
     d3.selectAll('.datamaps-arc')
       .data(filteredLinks)
-      .attr('id', function(link) {return [link.verb, link.source, link.target].join('-')})
-      .attr('class', function (link) {return 'datamaps-arc ' + link.verb});
+      .attr({
+        id: function(link) {return [link.verb, link.source, link.target].join('-')},
+        'class': function (link) {return 'datamaps-arc ' + link.verb}
+      });
 
     d3.select('#graph .arc').append('text');
 
@@ -100,10 +106,12 @@
 
       d3.select('#graph .arc text')
         .append('textPath')
-        .attr('xlink:href', '#' + [link.verb, link.source, link.target].join('-'))
-        .attr('class', 'datamaps-arc-label ' + link.verb)
-        .attr('title', [origin.name, link.verb, destination.name].join(' '))
-        .text([origin.name, link.verb, destination.name].join(' '));
+        .text([origin.name, link.verb, destination.name].join(' '))
+        .attr({
+          'xlink:href': '#' + [link.verb, link.source, link.target].join('-'),
+          'class': 'datamaps-arc-label ' + link.verb,
+          title: [origin.name, link.verb, destination.name].join(' ')
+        });
     });
 
     var $g = d3.selectAll('g'),
@@ -115,14 +123,91 @@
           $text.style('display', 'block');
         }).debounce(100);
 
+    var redrawAbstractArcs = _(function (e) {
+
+
+
+        }).debounce(100);
+
+    var previousScale = 1;
+    var handleZoomChanged = function () {
+          if (previousScale !== d3.event.scale) {hideText()}
+
+          $g.attr({transform: "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")"});
+
+          if (previousScale !== d3.event.scale) {redrawText(d3.event)}
+
+          redrawAbstractArcs();
+
+          previousScale = d3.event.scale;
+        };
+
     var zoom = d3.behavior.zoom()
-          .on("zoom", function () {
-            hideText();
-            $g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-            redrawText(d3.event);
-          });
+          .on("zoom", handleZoomChanged);
 
     mapGraph.svg.call(zoom);
+
+    d3.select('svg')
+      .append('g').attr({'class': 'abstract-nodes'})
+        .append('rect').attr({'class': 'abstract-node', x: 5, y: 5, height: 20, width: 150});
+
+
+
+    /*
+
+     * on zoom, find bottom of the viewport, draw a rectangle, then draw an arc "originating" from Entity,
+     * but ending up on translated pixel coordinates
+     *
+     * get pixel coordinates of present source/target, and arc to known coordinates of <rect>
+
+    */
+
+    // note: projection takes coordinates in [lng,lat] order
+
+//    d3.select('rect').attr({
+//      x: mapGraph.projection([-2.617156, 51.494355])[0],
+//      y: mapGraph.projection([-2.617156, 51.494355])[1]
+//    });
+
+
+    abstractArcs = mapGraph.svg.append('g')
+      .attr({'class': 'abstract-arcs'});
+
+    abstractArcs = abstractArcs.selectAll('.abstract-arc')
+      .data(abstractLinks, JSON.stringify);
+
+    abstractArcs
+      .enter()
+        .append('path')
+        .attr('class', 'abstract-arc')
+        .style('stroke-linecap', 'round')
+        .style('stroke', function(datum) {
+          if ( datum.options && datum.options.strokeColor) {
+            return datum.options.strokeColor;
+          }
+          return  arcOptions.strokeColor
+        })
+        .style('fill', 'none')
+        .style('stroke-width', function(datum) {
+          if ( datum.options && datum.options.strokeWidth) {
+            return datum.options.strokeWidth;
+          }
+          return arcOptions.strokeWidth;
+        })
+        .attr('d', function(datum) {
+          var originXY = mapGraph.latLngToXY(datum.origin.latitude, datum.origin.longitude);
+          var destXY = [5, 5]; //self.latLngToXY(datum.destination.latitude, datum.destination.longitude);
+          var midXY = [ (originXY[0] + destXY[0]) / 2, (originXY[1] + destXY[1]) / 2];
+          return "M" + originXY[0] + ',' + originXY[1] + "S" + (midXY[0] + (50 * arcOptions.arcSharpness)) + "," + (midXY[1] - (75 * arcOptions.arcSharpness)) + "," + destXY[0] + "," + destXY[1];
+        });
+
+    abstractArcs.exit()
+        .transition()
+        .style('opacity', 0)
+        .remove();
+
+
+
   });
 
 })();
