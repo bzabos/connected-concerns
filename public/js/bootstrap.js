720538affa4dcde7941fa9e1eb485d6362ae9113
bzabos <b.zabos@gmail.com>
2015-08-28 23:09:13 -0600

    Updated app to have labels in bubbles; fullscreen; reformatted js doc.

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -1,50 +1,84 @@
 (function () {
 
-  var width = 960,
-    height = 500;
+  var width = window.innerWidth,
+      height = window.innerHeight;
 
   var color = d3.scale.category20();
 
   var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
-    .size([width, height]);
+      .charge(-120)
+      .linkDistance(250)
+      .size([width, height]);
 
   var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+      .attr("width", width)
+      .attr("height", height);
 
-  d3.json("/json/graph.json", function(error, graph) {
+  d3.json("/json/graph.json", function (error, graph) {
     force
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .start();
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
 
     var link = svg.selectAll(".link")
-      .data(graph.links)
-      .enter().append("line")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value || 100); });
+        .data(graph.links)
+        .enter()
+          .append("line")
+          .attr("class", "link")
+          .style("stroke-width", function (d) {
+            return Math.sqrt(d.value || 50);
+          });
 
     var node = svg.selectAll(".node")
-      .data(graph.nodes)
-      .enter().append("circle")
-      .attr("class", "node")
-      .attr("r", 5)
-      .style("fill", function(d) { return color(d.id); })
-      .call(force.drag);
+        .data(graph.nodes)
+        .enter()
+          .append('g')
+          .attr("class", "node")
+          .attr("width", 100)
+          .attr("height", 100);
 
-    node.append("title")
-      .text(function(d) { return d.name; });
+    node
+          .append("rect")
+            .attr('height', 20)
+            .attr('width', 150)
+            .attr('rx', 5)
+            .attr('ry', 5)
+            .style("fill", function (d) {
+              return color(d.id);
+            });
 
-    force.on("tick", function() {
-      link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+    node
+        .append("text")
+            .attr('x', 2)
+            .attr('y', 18)
+            .style("fill", 'white')
+            .text(function (d) {
+              return d.name;
+            });
 
-      node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+    node
+        .call(force.drag);
+
+
+    force.on("tick", function () {
+      link
+          .attr("x1", function (d) {
+            return d.source.x;
+          })
+          .attr("y1", function (d) {
+            return d.source.y;
+          })
+          .attr("x2", function (d) {
+            return d.target.x;
+          })
+          .attr("y2", function (d) {
+            return d.target.y;
+          });
+
+      node
+          .attr("transform", function (d) {
+            return "translate(" + [d.x, d.y].join() + ")"
+          });
     });
   });
 
